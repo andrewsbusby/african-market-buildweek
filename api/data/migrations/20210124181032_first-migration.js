309ab85447fa955e36d94ff4caf72332a9585107
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-07-30 11:04:40 -0600

    update to migrations to have multiple tables

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -6,15 +6,29 @@ exports.up = async (knex) => {
       users.string('password', 200).notNullable()
       users.timestamps(false, true)
     })
-  await knex.schema
-    .createTable('country', (country) => {
-      country.increments('country_id')
-      country.string('country_name', 120).notNullable()
-      country.string('country_abr', 10)
-    })
+  await knex.schema.createTable('products', (tbl) => {
+    tbl.increments('product_id')
+    tbl.string('product_name', 200).notNullable()
+    tbl.string('product_description', 300).notNullable()
+  })
+  await knex.schema.createTable('listings', (tbl) => {
+    tbl.increments('listing_id')
+    tbl.integer('user_id')
+      .unsigned()
+      .references('users.user_id')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT')
+    tbl.string('listing_catagory', 200).notNullable()
+    tbl.string('listing_name', 200).notNullable()
+    tbl.string('listing_description', 300).notNullable()
+    tbl.string('listing_location', 150).notNullable()
+    tbl.decimal('listing_price', 10, 2).notNullable()
+  })
 }
 
 exports.down = async (knex) => {
   await knex.schema.dropTableIfExists('users')
-  await knex.schema.dropTableIfExists('country')
+  await knex.schema.dropTableIfExists('products')
+  await knex.schema.dropTableIfExists('listings')
+  
 }
