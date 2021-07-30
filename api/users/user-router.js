43c81a5c01e6fbb81a103f4461997a71dd3a8bf8
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-07-30 12:25:12 -0600

    update to user router

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -1,4 +1,18 @@
 const router = require('express').Router()
+const Users = require('../owners/owner-model');
 
+router.get('/', (req, res, next)=> {
+    Users.getListings()
+        .then(listings => {
+            if(listings){
+                res.status(200).json(listings)
+            } else {
+                res.status(400).json({
+                    message: 'no listings found'
+                })
+            }
+        })
+        .catch(next)
+})
 
 module.exports = router;
